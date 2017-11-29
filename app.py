5417f6b3106f9d3c464476d43756ec7bf0db9dde
Marcelo Mendonca <marcelomendonca84@gmail.com>
2017-11-29 17:00:54 -0500

    Added website support files

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/result')
+def result():
+   dict = {'phy':50,'che':60,'maths':70}
+   return render_template('result.html', result = dict)
 
 @app.route('/')
-def hello():
-    return "Hello World!"
-
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello Mrs. {}!".format(name)
+def hello_name():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+   app.run(debug = True)
